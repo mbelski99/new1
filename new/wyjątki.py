84432e6180edb365fe60a8bdbf4ce320a270d07d
mbelski99 <mbelski99@gmail.com>
2021-01-11 19:00:33 +0100

    JS

--- new/wyjątki.py
+++ new/wyjątki.py
@@ -1,8 +1,7 @@
 try:
     txt_file = open('plik.txt', 'w')
     txt_file.write('Wynik dzielenia: ')
-    division_result = int(input('dzielna: ')) /
-                        int(input('dzielnik: '))
+    division_result = int(input('dzielna: ')) / int(input('dzielnik: '))
     txt_file.write(str(division_result))
     txt_file.close()
 except (IOError, ZeroDivisionError) as e:
